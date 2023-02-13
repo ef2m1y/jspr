d94513d3fd47ecc0f4a6b5132d952213ab258f61
ef2m1y <shisotem01072002@gmail.com>
2023-02-13 16:30:53 +0900

    feat: fetch userInfo from my GitHub account

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,1 +1,8 @@
-console.log("index.js: loaded");
+const userId = "ef2m1y";
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+        console.log(response.status);
+        return response.json().then(userInfo => {
+            console.log(userInfo);
+        });
+    });
